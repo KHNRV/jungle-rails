998e12d8398995cc73018f424b35d85dcbd79577
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-04-05 16:52:55 -0400

    Create authentification method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, uniqueness: true
+  validates :email, uniqueness: {case_sensitive: false}
+  validates :password, confirmation: true, length: { minimum: 8 }
+
+  def authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email.strip.downcase)
+    return (@user && @user.authenticate(password) )? @user : nil
+  end
 end
